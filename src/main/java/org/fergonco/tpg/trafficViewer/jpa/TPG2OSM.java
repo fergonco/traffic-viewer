29c39d69c97d80039625feb062d8e29c9a2da070
Fernando González Cortés <fernando.gonzalez@geomati.co>
2017-01-18 18:12:58 +0100

    skeleton to populate tpg2osm table

--- src/main/java/org/fergonco/tpg/trafficViewer/jpa/TPG2OSM.java
+++ src/main/java/org/fergonco/tpg/trafficViewer/jpa/TPG2OSM.java
@@ -17,6 +17,24 @@ public class TPG2OSM {
 		private String line;
 		private String direction;
 		private String stopName;
+
+		public Key() {
+		}
+
+		public Key(String line, String direction, String stopName) {
+			super();
+			this.line = line;
+			this.direction = direction;
+			this.stopName = stopName;
+		}
+
 	}
 
+	public void setKey(TPG2OSM.Key key) {
+		this.key = key;
+	}
+
+	public void setOsmids(long[] osmids) {
+		this.osmids = osmids;
+	}
 }
