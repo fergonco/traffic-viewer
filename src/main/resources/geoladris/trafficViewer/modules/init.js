e147f97ea1692bac15cb1f409ccd5393a67a3f7d
Fernando González Cortés <fernando.gonzalez@geomati.co>
2017-01-19 22:00:25 +0100

    refresh map regularly

--- src/main/resources/geoladris/trafficViewer/modules/init.js
+++ src/main/resources/geoladris/trafficViewer/modules/init.js
@@ -1,5 +1,5 @@
-define([], function(){
-   
-      console.log("boh");
-   
+define(["message-bus"], function(bus) {
+   setInterval(function() {
+      bus.send("redraw-layer", "osmspeeds");
+   }, 5000);
 });
