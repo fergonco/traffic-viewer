52ab6d892d1413673735a74497e9760b914abdc7
Fernando González Cortés <fernando.gonzalez@geomati.co>
2017-02-06 18:01:27 +0100

    adaptations to the new ol2 plugin

--- src/main/resources/geoladris/trafficViewer/modules/init.js
+++ src/main/resources/geoladris/trafficViewer/modules/init.js
@@ -9,17 +9,33 @@ define([ "message-bus" ], function(bus) {
    }, 5000);
 
    bus.listen("modules-loaded", function(e, message) {
-      bus.send("add-layer", {
-         "id" : "meteo-eeuu",
-         "groupId" : "landcover",
-         "label" : "Radar EEUU",
-         "active" : "true",
-         "mapLayers" : [ {
+      bus.send("map:addLayer", {
+         "layerId" : "osm_speeds",
+         "wms" : {
             "baseUrl" : "http://localhost:6305/geoserver/wms",
             "wmsName" : "tpg:osm_speeds"
-         } ]
+         }
       });
-      bus.send("layers-loaded");
-      bus.send("layer-visibility", ["meteo-eeuu", true]);
+      bus.send("zoom-to", {
+         "x" : 6.03,
+         "y" : 46.24,
+         "zoomLevel" : 14
+      });
+      // portal.properties
+      // map.centerLonLat=6.03,46.24
+      // map.initialZoomLevel=14
+
+      // bus.send("add-layer", {
+      // "id" : "meteo-eeuu",
+      // "groupId" : "landcover",
+      // "label" : "Radar EEUU",
+      // "active" : "true",
+      // "mapLayers" : [ {
+      // "baseUrl" : "http://localhost:6305/geoserver/wms",
+      // "wmsName" : "tpg:osm_speeds"
+      // } ]
+      // });
+      // bus.send("layers-loaded");
+      // bus.send("layer-visibility", ["meteo-eeuu", true]);
    });
 });
