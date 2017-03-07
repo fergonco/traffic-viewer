70d63ff952e7078fa5a281956cfe16e9e595a03e
Fernando González Cortés <fernando.gonzalez@geomati.co>
2017-03-07 15:27:25 +0100

    legend templating through public-conf.json

--- viewer/src/main/resources/geoladris/trafficViewer/modules/legend.js
+++ viewer/src/main/resources/geoladris/trafficViewer/modules/legend.js
@@ -1,13 +1,13 @@
-define([ "message-bus", "ui/ui", "./text!./legend.html" ], function(bus, ui, template) {
+define([ "message-bus", "ui/ui", "module" ], function(bus, ui, module) {
 
    var ON = "VisibleLegendContent";
    var OFF = "HiddenLegendContent";
 
    var temp = document.createElement('div');
-   temp.innerHTML = template;
+   temp.innerHTML = module.config().template;
    var panel = temp.firstChild;
    document.body.append(panel);
-   
+
    var button = document.getElementById("LegendTab");
    button.addEventListener("click", function() {
       if (panel.className == ON) {
