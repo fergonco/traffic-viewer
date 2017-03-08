fc8d69a6012dc167d2e86d1abaa772bbf969ef16
Fernando González Cortés <fernando.gonzalez@geomati.co>
2017-03-08 18:14:29 +0100

    timeline working

--- viewer/src/main/resources/geoladris/trafficViewer/modules/timeline.js
+++ viewer/src/main/resources/geoladris/trafficViewer/modules/timeline.js
@@ -1,76 +1,155 @@
 define(
    [ "message-bus", "Mustache" ],
    function(bus, Mustache) {
+
+      var timestamps = [];
+      var scaleFactor = 0.0001;
+      var minTimestamp = null;
+      var maxTimestamp = null;
+      var selectedTimestamp = null;
+      var interval = null;
+
       var template = '\
-       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"\
-         height="100%" width="100%">\
-         <!-- background -->\
-         <rect id="TimelineRect" x="0" y="0" height="100%" width="100%"\
-            style="stroke:#000000; fill: #eeeeee" />\
-         <!-- one layer timeline -->\
-         <line x1="0%" x2="100%" y1="50%" y2="50%" style="stroke:#ff0000" />\
-         {{#timestamps}}\
-           <circle cx="{{x}}%" cy="50%" r="5" style="fill: #00ccff;" />\
-           {{#text}}\
-             <text x="{{x}}%" y="45%" transform="rotate(-35)" style="transform-origin: {{x}}% 45%; font-size:xx-small">{{.}}</text>\
-           {{/text}}\
-         {{/timestamps}}\
-       </svg>';
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"\
+          height="100%" width="{{totalWidth}}">\
+          <!-- background -->\
+          <rect id="TimelineRect" x="0" y="0" height="100%" width="100%"\
+             style="stroke:#000000; fill: #eeeeee" />\
+          <!-- selection -->\
+          <rect id="TimelineSelection" x="0%" y="5%" width="50" height="90%" stroke-dasharray="5, 5" style="stroke:#ff0000; fill: none" />\
+          <!-- one layer timeline -->\
+          <line x1="0%" x2="100%" y1="50%" y2="50%" style="stroke:#ff0000; pointer-events:none" />\
+          {{#timestamps}}\
+            <circle cx="{{x}}" cy="50%" r="5" style="fill: #00ccff; pointer-events:none" />\
+            {{#text}}\
+              <text x="{{x}}" y="40%" text-anchor="middle" style="pointer-events:none">{{.}}</text>\
+            {{/text}}\
+          {{/timestamps}}\
+        </svg>\
+      ';
 
-      // <!-- current timestamp -->\
-      // <line x1="38%" x2="38%" y1="0%" y2="100%" stroke-dasharray="5, 5"\
-      // style="stroke:#0000ff" />\
-      // <!-- mouseover -->\
-      // <circle cx="64%" cy="50%" r="10" style="fill: #7777ff;/>';
-
-      var timeline = document.createElement('div');
-      timeline.id = "TimeLine";
-      document.body.append(timeline);
+      var container = document.createElement("div");
+      container.id = "Timeline";
+      document.body.append(container);
 
       bus.listen("add-timestamps", function(e, message) {
          var dateTimestamps = message.timestamps;
-         var timestamps = [];
-         var min = Number.MAX_VALUE;
-         var max = Number.MIN_VALUE;
+         timestamps = [];
+         minTimestamp = Number.MAX_VALUE;
+         maxTimestamp = Number.MIN_VALUE;
          for (var i = 0; i < dateTimestamps.length; i++) {
             var timestamp = new Date(dateTimestamps[i]).getTime();
             timestamps.push(timestamp);
-            if (timestamp < min) {
-               min = timestamp;
+            if (timestamp < minTimestamp) {
+               minTimestamp = timestamp;
             }
-            if (timestamp > max) {
-               max = timestamp;
+            if (timestamp > maxTimestamp) {
+               maxTimestamp = timestamp;
             }
          }
-         var range = max - min;
+         applyTemplate();
+         setNearestTime(new Date().getTime());
+      });
+
+      function applyTemplate() {
          var timestampData = [];
+         var containerBounds = container.getBoundingClientRect();
          for (var i = 0; i < timestamps.length; i++) {
             var timestamp = timestamps[i];
-            var percent = 100 * (timestamp - min) / range;
+            var x = (timestamp - minTimestamp) * scaleFactor + containerBounds.width / 2;
             var date = new Date(timestamp);
             var timestampEntry = {
-               "x" : percent
-            }
-            if (i % 4 == 0) {
-               timestampEntry.text = date.getHours() + "h" + date.getMinutes();
+               "x" : x,
+               text : date.getHours() + "h" + ("0" + date.getMinutes()).slice(-2)
             }
             timestampData.push(timestampEntry);
          }
 
          var view = {
-            "timestamps" : timestampData
+            "timestamps" : timestampData,
+            "totalWidth" : (maxTimestamp - minTimestamp) * scaleFactor + containerBounds.width
+
          }
-         timeline.innerHTML = Mustache.render(template, view);
+         container.innerHTML = Mustache.render(template, view);
 
          var rect = document.getElementById("TimelineRect");
          rect.addEventListener("click", function(event) {
             var rect = this.getBoundingClientRect();
             var x = event.clientX - rect.left;
-            var ratioWidth = x / rect.width;
-            var time = ratioWidth * range + min;
-            var date = new Date(time);
-            console.log(date.getHours() + "h" + date.getMinutes());
+            var xForMin = containerBounds.width / 2;
+            var time = ((x - xForMin) / scaleFactor) + minTimestamp;
+            setNearestTime(time);
          });
-      });
+
+         updateSelection();
+      }
+
+      function setNearestTime(time) {
+         // Find nearest timestamp
+         var nearestTimestamp = null;
+         var nearestDistance = Number.MAX_VALUE;
+         for (var i = 0; i < timestamps.length; i++) {
+            var distance = Math.abs(timestamps[i] - time);
+            if (nearestTimestamp == null || distance < nearestDistance) {
+               nearestDistance = distance;
+               nearestTimestamp = timestamps[i];
+            }
+         }
+
+         selectedTimestamp = nearestTimestamp;
+         
+         bus.send("timeline:selection", {
+            "timestamp" : selectedTimestamp
+         });
+
+         updateSelection();
+      }
+
+      function updateSelection() {
+         if (selectedTimestamp != null) {
+            // Scroll to it
+            var current = container.scrollLeft;
+            var target = (selectedTimestamp - minTimestamp) * scaleFactor;
+            animate({
+               delay : 20,
+               duration : 500,
+               delta : function(progress) {
+                  return progress;
+               },
+               step : function(delta) {
+                  container.scrollLeft = current + (target - current) * delta;
+                  if (delta == 1) {
+                     // center selection
+                     var containerBounds = container.getBoundingClientRect();
+                     var timelineSelection = document.getElementById("TimelineSelection");
+                     timelineSelection.setAttribute("x", container.scrollLeft + containerBounds.width / 2 - 25);
+                  }
+               }
+            });
+         }
+      }
+
+      window.onresize = applyTemplate;
+
+      function animate(opts) {
+
+         var start = new Date();
+
+         var id = setInterval(function() {
+            var timePassed = new Date - start
+            var progress = timePassed / opts.duration
+
+            if (progress > 1)
+               progress = 1
+
+            var delta = opts.delta(progress)
+            opts.step(delta)
+
+            if (progress == 1) {
+               clearInterval(id)
+            }
+         }, opts.delay || 10)
+
+      }
 
    });
