3563db28f01899245625877f19b00b117fb50f2c
Fernando González Cortés <fernando.gonzalez@geomati.co>
2017-04-07 11:14:14 +0200

    all scatterplots

--- analyzer/analyse/eda-tocern.r
+++ analyzer/analyse/eda-tocern.r
@@ -1,20 +1,33 @@
 library(ggplot2);
 
 speeds <-read.csv("tocern.csv")
-summary (speeds$speed)
-boxplot(speeds$speed)
-hist(speeds$speed)
 
-scatterplotSpeedMinutes <- ggplot(data = speeds, aes(x=minutesDay, y=speed)) + geom_point(shape=1)
-print(scatterplotSpeedMinutes)
-scatterplotSpeedTimestamp <- ggplot(data = speeds, aes(x=timestamp, y=speed)) + geom_point(shape=1)
-print(scatterplotSpeedTimestamp)
-scatterplotSpeedSchoolfr <- ggplot(data = speeds, aes(x=schoolfr, y=speed)) + geom_point(shape=1)
-print(scatterplotSpeedSchoolfr)
+scatterplots <- function() {
+  for (i in names(speeds)) {
+    scatterplot <- ggplot(data = speeds, aes_string(x=i, y="speed")) + geom_point(shape=1)
+    print(scatterplot)
+  }
+}
 
-facetedHistograms <- ggplot(data = speeds, aes(x=speed)) + geom_histogram() + ggtitle("results") + facet_grid(morningrush~weekday, labeller = label_both)
-print(facetedHistograms)
-facetedScatterplots <- ggplot(data = speeds, aes(x=minutesDay, y=speed)) + geom_point(shape=1) + ggtitle("results") + facet_grid(weekday~morningrush, labeller = label_both)
-print(facetedScatterplots)
-facetedScatterplots <- ggplot(data = speeds, aes(x=temperature, y=speed)) + geom_point(shape=1) + ggtitle("results") + facet_grid(weekday~morningrush, labeller = label_both)
-print(facetedScatterplots)
+scatterplots()
+
+
+
+
+# summary (speeds$speed)
+# boxplot(speeds$speed)
+# hist(speeds$speed)
+# 
+# scatterplotSpeedMinutes <- ggplot(data = speeds, aes(x=minutesDay, y=speed)) + geom_point(shape=1)
+# print(scatterplotSpeedMinutes)
+# scatterplotSpeedTimestamp <- ggplot(data = speeds, aes(x=timestamp, y=speed)) + geom_point(shape=1)
+# print(scatterplotSpeedTimestamp)
+# scatterplotSpeedSchoolfr <- ggplot(data = speeds, aes(x=schoolfr, y=speed)) + geom_point(shape=1)
+# print(scatterplotSpeedSchoolfr)
+# 
+# facetedHistograms <- ggplot(data = speeds, aes(x=speed)) + geom_histogram() + ggtitle("results") + facet_grid(morningrush~weekday, labeller = label_both)
+# print(facetedHistograms)
+# facetedScatterplots <- ggplot(data = speeds, aes(x=minutesDay, y=speed)) + geom_point(shape=1) + ggtitle("results") + facet_grid(weekday~morningrush, labeller = label_both)
+# print(facetedScatterplots)
+# facetedScatterplots <- ggplot(data = speeds, aes(x=temperature, y=speed)) + geom_point(shape=1) + ggtitle("results") + facet_grid(weekday~morningrush, labeller = label_both)
+# print(facetedScatterplots)
